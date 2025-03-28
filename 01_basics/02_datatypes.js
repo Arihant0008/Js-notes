c5a7a44705d45e813085f77f2d5b37cada5468d0
“Arihant0008” <jain.arihant0008@gmail.com>
2025-03-28 18:51:47 +0530

    Datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict";  // treat all JS code as newer version
+
+// alert("Hello") // we are using node js not browser
+
+console.log(3+3) 
+console.log("Hi")
+
+
+let name = "ashu"
+let age = 18
+let isLoggedIn = false
+
+
+/*
+String - "",''.``;
+Number - 1,90,20.6,Infinity,NaN
+BigInt - 45n,2^53-1
+boolean - false, true
+Undefined - memory le li hai us memory ke andar hai nahi kuch
+Null - not exist
+Symbol- use for saving unique values
+
+
+--Non - premitive
+Array - Non primitive
+Object: {key: "value"}
+*/
