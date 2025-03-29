eeb109c670d71af2a146193a3105e5103a364073
“Arihant0008” <jain.arihant0008@gmail.com>
2025-03-29 13:14:44 +0530

    Basics of String

---
+++ 01_basics/05_string.js
@@ -0,0 +1,41 @@
+const name = "Ashu"
+const repoCount = 50
+
+// console.log(name + repoCount + " Value");
+
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gamename = new String("Arihant-Jain")
+
+// console.log(gamename[0]);
+// console.log(gamename.__proto__);
+
+
+// console.log(gamename.length);
+// console.log(gamename.toUpperCase());
+// console.log(gamename.charAt(2));
+// console.log(gamename.indexOf('t'));
+
+
+const newString = gamename.substring(-8,4)
+// console.log(newString);
+
+const anotherString = gamename.slice(-11,4)
+// console.log(anotherString);
+
+const newStringOne = "   Arihant    "
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
+
+const url = "https://ashu.com/ashu%20jain"
+
+// console.log(url.replace('%20','-'));
+
+// console.log(url.includes('ashu'))
+
+// console.log(gamename.split('-'));
+
+// console.log(gamename.at(2))
+console.log(newStringOne.trimStart())
+
+
