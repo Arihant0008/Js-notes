cd7889be9a0c3fd58520c6da09433606e6e226f7
“Arihant0008” <jain.arihant0008@gmail.com>
2025-03-29 11:08:36 +0530

    conversion of datatypes

--- 01_basics/03_conversionOperator.js
+++ 01_basics/03_conversionOperator.js
@@ -26,7 +26,43 @@ let booleanNo = Boolean(isLoggedIn)
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// ***** opreations *****
+
+let value = 3
+
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%3);
+
+let str1 = "Ashu"
+let str2 = "Jain"
+
+let str3 = str1+str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1"+ 2+ 2);
+// console.log(2 + 1 + "2");
+
+console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+// num1 = num2 = num3 = 2 + 2
+
+let gamecounter = 100
+gamecounter++
+console.log(gamecounter);
 
 
