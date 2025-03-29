23650341c2ff9c3af9a64fe60141b8513b3b7ade
“Arihant0008” <jain.arihant0008@gmail.com>
2025-03-29 10:44:35 +0530

    conversion of datatypes

---
+++ 01_basics/03_conversionOperator.js
@@ -0,0 +1,33 @@
+// let score = "33abc"
+// let score = null
+let score = undefined
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+// true =>1 false =>0
+
+let isLoggedIn = ""
+
+let booleanNo = Boolean(isLoggedIn)
+// console.log(booleanNo);
+
+// 1=> true 0=> false
+// ""=> false
+// "Ashu"=> true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+
